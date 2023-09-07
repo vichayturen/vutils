b01255a3ab64b2e263963bee568fcd12f84a5c87
guogaipinkpig <824058109@qq.com>
2023-09-07 17:21:16 +0800

    update logger

--- myutils/logger.py
+++ myutils/logger.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(process)d --- [%(threadName)s] %(filename)s-%(funcName)s: %(message)s",
+    format="%(asctime)s %(levelname)8s %(process)d --- [%(threadName)s] %(filename)s-%(funcName)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
