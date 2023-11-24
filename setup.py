a312302b8c3a7da8905066f68ce07010a5d5a750
RankKCodeTalker <1073931273@qq.com>
2023-11-24 15:16:57 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vutils",
-    version="1.0.7.dev",
+    version="1.0.7",
     author="Vichayturen",
     author_email="vichayturen@gmail.com",
     description="vichayturen's utils",
