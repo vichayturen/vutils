6e458d1e9e74b30fda620bd2876d15ed9492a4e4
RankKCodeTalker <1073931273@qq.com>
2023-11-24 15:16:22 +0800

    change dir

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vtils",
-    version="1.0.6.dev",
+    name="vutils",
+    version="1.0.7.dev",
     author="Vichayturen",
     author_email="vichayturen@gmail.com",
     description="vichayturen's utils",
 
-    # 项目主页
-    url="", 
-    # 表明当前模块依赖哪些包，若环境中没有，则会从pypi中下载安装
+    url="https://github.com/RankKCodeTalker/myutils", 
+    license="MIT",
     install_requires=[],
-    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
-    packages=find_packages()
+    package_dir={"": "src"},
+    packages=find_packages("src"),
 )
