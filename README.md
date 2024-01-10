9bf3de22b45936a2ad47c26fb97918ad4926b640
Vichayturen <1073931273@qq.com>
2024-01-10 14:18:55 +0800

    update timer and README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# myutils
-我的python常用函数库
+# vutils
+
+## 使用方法
+
+### 文件读写
+包括json，jsonl，csv，txt
+```python
+from vutils import io
+
+data = io.jsonload("data.json")
+# do something
+io.jsondump(data, "result.json")
+```
+
+### Excel文件读写
+```python
+from vutils.io.excel_handler import ExcelHandler
+
+excel = ExcelHandler("./test.xlsx")
+question = excel.getValue(1, '问题')
+answer = "yes"
+excel.setValue(1, '答案', answer)
+excel.saveAs("./result.xlsx")
+```
+
+### 日志
+```python
+from vutils.log import logger
+
+logger.info("你好")
+logger.warning("你好")
+logger.error("你好")
+```
+
+### 网络代理
+```python
+from vutils.net import set_proxy, unset_proxy
+
+set_proxy(port=7890)
+# do something
+unset_proxy()
+```
+
+### 计时器
+```python
+import time
+import random
+from vutils.timer import get_timer
+
+with get_timer(log_file_path="./log.json") as t:
+    for i in range(10):
+        t.label("生成随机数")
+        ts = random.random()
+        t.label("打印随机数")
+        print(ts)
+        t.label("等待时间")
+        time.sleep(ts)
+```
--- src/vutils/timer.py
+++ src/vutils/timer.py
@@ -10,7 +10,7 @@ class MyTimer:
         self.now_work = init_work
         self.statistic_result = {
             init_work: {
-                "runtime": 0,
+                "total_runtime": 0,
                 "max_runtime": 0,
                 "min_runtime": float('inf'),
                 "iter": 0,
@@ -30,10 +30,10 @@ class MyTimer:
             self.statistic_result[self.now_work]['max_runtime'] = last_time
         if last_time < self.statistic_result[self.now_work]['min_runtime']:
             self.statistic_result[self.now_work]['min_runtime'] = last_time
-        self.statistic_result[self.now_work]['runtime'] += last_time
+        self.statistic_result[self.now_work]['total_runtime'] += last_time
         self.statistic_result[self.now_work]['iter'] += 1
         for k, v in self.statistic_result.items():
-            self.statistic_result[k]['average_runtime'] = v['runtime']/v['iter']
+            self.statistic_result[k]['average_runtime'] = v['total_runtime']/v['iter']
         print(f"日志文件保存在{self.log_file_path}")
         io.jsondump(self.statistic_result, self.log_file_path)
         return False
@@ -47,11 +47,11 @@ class MyTimer:
             self.statistic_result[self.now_work]['max_runtime'] = last_time
         if last_time < self.statistic_result[self.now_work]['min_runtime']:
             self.statistic_result[self.now_work]['min_runtime'] = last_time
-        self.statistic_result[self.now_work]['runtime'] += last_time
+        self.statistic_result[self.now_work]['total_runtime'] += last_time
         self.statistic_result[self.now_work]['iter'] += 1
         if work not in self.statistic_result:
             self.statistic_result[work] = {
-                "runtime": 0,
+                "total_runtime": 0,
                 "max_runtime": 0,
                 "min_runtime": float('inf'),
                 "iter": 0,
