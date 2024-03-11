c52a0ff63a8c600e84313e173a7b356cd1060496
Vichayturen <1073931273@qq.com>
2024-03-11 11:27:49 +0800

    update metadata

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Vichayturen",
     author_email="vichayturen@gmail.com",
     description="vichayturen's utils",
-    url="https://github.com/RankKCodeTalker/myutils",
+    url="https://github.com/RankKCodeTalker/vutils",
     license="MIT",
     install_requires=[],
     extra_requires=[
